bfa753ce4eb8d76fbd076c3a25a05e9b1b9533e3
redfire29 <redfire29@gmail.com>
2025-01-20 15:35:54 +0800

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,11 +59,11 @@ export default defineNuxtConfig({
         // { name: 'apple-mobile-web-app-title', content: '君華御品', },
       ],
       link:[
-        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96', },
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', },
-        { rel: 'shortcut icon', href: '/favicon.ico', },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png', },
-        { rel: 'manifest', href: '/site.webmanifest', },
+        { rel: 'icon', type: 'image/png', href: '/curva-umi-support/favicon-96x96.png', sizes: '96x96', },
+        { rel: 'icon', type: 'image/svg+xml', href: '/curva-umi-support/favicon.svg', },
+        { rel: 'shortcut icon', href: '/curva-umi-support/favicon.ico', },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/curva-umi-support/apple-touch-icon.png', },
+        { rel: 'manifest', href: '/curva-umi-support/site.webmanifest', },
       ],
       script: [],
     }
